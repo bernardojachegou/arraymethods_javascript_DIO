2f95d1912a0c60bb6072d149eafb25a23dbe3dd0
Michel Bernardo <bernardoenglish@outlook.com>
2020-10-28 14:51:59 -0400

    method map studied

---
+++ map.js
@@ -0,0 +1,53 @@
+const pets = [
+    {
+        type: 'dog',
+        name: 'bolinha',
+        age: 15,
+        weight: 30
+    },
+    {
+        type: 'cat',
+        name: 'mingal',
+        age: 6,
+        weight: 10
+    },
+    {
+        type: 'dog',
+        name: 'rex',
+        age: 4,
+        weight: 22
+    },
+    {
+        type: 'cat',
+        name: 'bolt',
+        age: 1,
+        weight: 5
+    },
+    {
+        type: 'fish',
+        name: 'nemo',
+        age: 2,
+        weight: 0.01
+    },
+    {
+        type: 'horse',
+        name: 'brown',
+        age: 7,
+        weight: 140
+    },
+]
+
+// Return a new array with the selected values;
+
+const returnNames = (pet) => pet.name;
+const petNames = pets.map((pet) => returnNames(pet));
+
+const simplePetNames = pets.map((pet) => {
+    return pet.age;
+});
+
+// forEach doesn't return a new array, we must push the selected items to the same array;
+
+console.log(pets);
+console.log(petNames);
+console.log(simplePetNames);
---
+++ reduce.js
@@ -0,0 +1,38 @@
+const pets = [
+    {
+        type: 'dog',
+        name: 'bolinha',
+        age: 15,
+        weight: 30
+    },
+    {
+        type: 'cat',
+        name: 'mingal',
+        age: 6,
+        weight: 10
+    },
+    {
+        type: 'dog',
+        name: 'rex',
+        age: 4,
+        weight: 22
+    },
+    {
+        type: 'cat',
+        name: 'bolt',
+        age: 1,
+        weight: 5
+    },
+    {
+        type: 'fish',
+        name: 'nemo',
+        age: 2,
+        weight: 0.01
+    },
+    {
+        type: 'horse',
+        name: 'brown',
+        age: 7,
+        weight: 140
+    },
+]
